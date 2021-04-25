e12594265ac53bd328467a7577a49727cb3645cb
Justo Enrique Mansilla <jmansilla@udesa.edu.ar>
2021-04-24 22:28:56 -0300

    .

---
+++ integrador/src/Componentes/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Footer extends Component {
+    render () {
+        return(
+     <h3>
+    Mansilla - Romero - Groszmann
+    </h3>
+        );
+    };
+};
+
+export default Footer;
---
+++ integrador/src/Componentes/Header.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Header extends Component {
+    render () {
+        return(
+            <h1>
+            Tarjeta de personas <span>Characters</span> 
+            </h1>
+        );
+    };
+};
+
+export default Header;
