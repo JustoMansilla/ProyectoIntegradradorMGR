9ca3b6059e6bfc86d970371350b91ce2f49bca3b
Justo Enrique Mansilla <jmansilla@udesa.edu.ar>
2021-04-27 01:10:16 -0300

    Filtrado por nombre, edad, nacionalidad

--- integrador/src/App.js
+++ integrador/src/App.js
@@ -41,6 +41,47 @@ export default class App extends Component {
       this.setState({arrayTarjetas: this.state.resetear})
     }
 
+    filtrarTarjetas(){
+      let filterData = document.querySelector(".filterData").value
+      let filtrarPor = document.querySelector(".filterBy").value
+      console.log(filterData)
+      console.log(filtrarPor)
+     
+      if(filterData != ""){
+
+          if (filtrarPor === "años"){
+              let resultado = this.state.arrayTarjetas.filter( (item)=> {
+                  let años = item.dob.age
+                  let años2 = años.toString()
+                   return años2.includes(filterData);
+              })
+              this.setState({arrayTarjetas: resultado});
+              console.log(this.state.arrayTarjetas)
+          }
+          else if(filtrarPor === "nombre"){
+              let filterDataMayus = filterData.toUpperCase()
+              
+
+              let resultado = this.state.arrayTarjetas.filter( (item)=> {
+                  let itemDataMayus = item.name.first.toUpperCase()
+                  return itemDataMayus.includes(filterDataMayus);
+              })
+              this.setState({arrayTarjetas: resultado});
+          }
+           else if(filtrarPor == "nacionalidad"){
+              let filterDataMayus = filterData.toUpperCase()            
+              let resultado = this.state.arrayTarjetas.filter( (item)=> {
+                  let itemDataMayus = item.location.country.toUpperCase()
+                  return itemDataMayus.includes(filterDataMayus);
+              })
+              this.setState({arrayTarjetas: resultado});
+           }
+      }
+      else{
+          this.setState({
+              arrayTarjetas: this.state.resetear
+          })
+      }}
 
 
   render() {
@@ -55,13 +96,23 @@ export default class App extends Component {
 
       <div className="Agregartarjeta"> 
         <div>
-          Cuantos usuarios quieres añadir?
+        <h5>Cuantos usuarios quieres añadir?</h5>
         </div>  
         <input type="number" className="cantidadTarjetas"  placeholder="Cantidad de usuarios a añadir"/>
           <button className="botonAgregar" onClick={() => this.agregarTarjetas(document.querySelector('.cantidadTarjetas').value)}>
               Añadir
           </button>
       </div>
+
+      <div className="seccionFiltro">
+        <h5>Filtrar por:</h5>
+          <select style={{padding: "0.5%"}} className="filterBy" name="filterBy">
+            <option value="años">Edad</option>
+            <option value="nombre">Nombre</option>
+            <option value="nacionalidad">Nacionalidad</option>
+          </select>
+        <input style={{margin: "1%", padding: "0.5%"}} onInput={this.filtrarTarjetas.bind(this)} className="filterData" name="filterData" type= "text"/>
+     </div>
   
 
       <div className="grid-container">
