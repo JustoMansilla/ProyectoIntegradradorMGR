4de969f0835b14cefc903648b49606a252297819
Justo Enrique Mansilla <jmansilla@udesa.edu.ar>
2021-04-27 12:43:44 -0300

    Cambio de color tarjetas onMouseEnter

--- integrador/src/Componentes/Tarjetas.js
+++ integrador/src/Componentes/Tarjetas.js
@@ -1,10 +1,34 @@
 import React, {Component} from 'react'
 
 class Tarjetas extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            color1: props.color,              
+        }
+    }
+
+    colorEnter(){
+        if(this.state.color === "#lightgray")
+        this.setState({
+            color: "#lightgray",
+        })
+        else
+        this.setState({
+            color: "lightgray",
+        })
+    }
+
+   
+
     render () {
         return(
        
-        <div className="Personaje-1">
+        <div className="Personaje-1"
+        style = {{backgroundColor: this.state.color}}
+        onMouseEnter={this.colorEnter.bind(this)}
+        >
 
             <div className="img-wrapper">    
                 <img className="inner-img" src={this.props.elemento.picture.large} alt="" />
